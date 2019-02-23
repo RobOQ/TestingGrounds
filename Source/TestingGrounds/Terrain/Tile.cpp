6be26e99f08cb046c8f92140c08f077f4b65eb1f
Rob O'Quinn <rob.oquinn@gmail.com>
2019-02-23 17:54:32 -0400

    Adding a CastSphere method to check for collisions within a radius. Destroying attached actors when a tile is destroyed.

--- Source/TestingGrounds/Terrain/Tile.cpp
+++ Source/TestingGrounds/Terrain/Tile.cpp
@@ -2,13 +2,13 @@
 
 #include "Tile.h"
 #include "Engine/World.h"
+#include "DrawDebugHelpers.h"
 
 // Sets default values
 ATile::ATile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn)
@@ -32,13 +32,52 @@ void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn)
 void ATile::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	CastSphere(GetActorLocation(), 300.0f);
+	CastSphere(GetActorLocation() + FVector(0.0f, 0.0f, 1000.0f), 300.0f);
 }
 
 // Called every frame
 void ATile::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
+void ATile::Destroyed()
+{
+	Super::Destroyed();
+
+	TArray<AActor*> AttachedActors;
+	GetAttachedActors(AttachedActors);
+	for(int i = 0; i < AttachedActors.Num(); ++i)
+	{
+		AttachedActors[i]->Destroy();
+	}
+}
+
+bool ATile::CastSphere(FVector Location, float Radius)
+{
+	FHitResult HitResult;
+	bool HasHit = GetWorld()->SweepSingleByChannel(
+		HitResult,
+		Location,
+		Location,
+		FQuat::Identity,
+		ECollisionChannel::ECC_GameTraceChannel2,
+		FCollisionShape::MakeSphere(Radius)
+	);
+
+	FColor ResultColor = HasHit ? FColor::Red : FColor::Green;
+
+	DrawDebugSphere(
+		GetWorld(),
+		Location,
+		Radius,
+		16,
+		ResultColor,
+		true,
+		100.0f
+	);
+
+	return HasHit;
+}
--- Source/TestingGrounds/Terrain/Tile.h
+++ Source/TestingGrounds/Terrain/Tile.h
@@ -18,12 +18,16 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Generation")
 	void PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn);
 
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void Destroyed() override;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:
+	bool CastSphere(FVector Location, float Radius);
 
 };
